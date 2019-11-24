c352982b8d8d85398c03da7e802c6fa81c3859b8
akikgt <komoria@oregonstate.edu>
2019-11-24 22:52:33 +0900

    Update FinalProject-BugFree folder

---
+++ projects/FinalProject-BugFree/dominion/unittest10.c
@@ -0,0 +1,54 @@
+/* -----------------------------------------------------------------------
+ * Unit test for bug10
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+static void assertEqual(int actual, int expected, char *msg) {
+    printf("%s: ", msg);
+    if (actual == expected) {
+        printf("PASS!: got %d\n", actual);
+        return;
+    }
+    else {
+        printf("FAILED: %d expected, but got %d.\n", expected, actual);
+        return;
+    }
+}
+
+int main() {
+    struct gameState G;
+    int currentPlayer = 0;
+    int nextPlayer = 1;
+
+    // set kingdom cards
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+
+    printf ("--------------------------\n");
+    printf ("--- UNIT TEST 10 STARTS ---\n");
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    initializeGame(2, k, 1000, &G); // initialize a new game
+
+    int currentPlayerHand[5] = {ambassador, silver, silver, gold, estate};
+    memcpy(G.hand[0], currentPlayerHand, sizeof(int) * 5); // set player's hand
+    int handCountBefore = G.handCount[currentPlayer];
+    int nextPlayerDiscardCountBefore = G.discardCount[nextPlayer];
+
+    int result = cardEffect(ambassador, 1, 1, 0, &G, 0, NULL);
+    assertEqual(result, 0, "Check cardEffect returns 0");
+    assertEqual(G.handCount[currentPlayer], handCountBefore - 2, "Check current player's handCount");
+    assertEqual(G.discardCount[nextPlayer], nextPlayerDiscardCountBefore + 1, "Check next player's discardCount");
+    if (G.discardCount[nextPlayer] - 1 >= 0)
+        assertEqual(G.discard[nextPlayer][G.discardCount[nextPlayer] - 1], silver, "Check the top card of next player's discard pile");
+
+    printf("--- UNIT TEST 10 ENDS ---\n");
+    printf("--------------------------\n");
+    return 0;
+}
